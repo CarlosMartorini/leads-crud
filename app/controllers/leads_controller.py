2a1f04b2db6c3f6723e5b098a83d22ff2409515e
Carlos Martorini <c7.silveira@gmail.com>
2021-09-26 18:34:16 -0300

    fix: Clean all TODO's and change .env to .env.example

--- app/controllers/leads_controller.py
+++ app/controllers/leads_controller.py
@@ -16,7 +16,6 @@ def create_lead():
         data['last_visit'] = datetime.now()
         data['visits'] = 1
 
-        # TODO: phone obrigatório no formato (xx)xxxxx-xxxx usar regex
         if fullmatch(phone_validation, data['phone']):
             return {'msg': f"The phone must be like (xx)xxxxx-xxxx and you try pass {data['phone']}"}
 
@@ -34,7 +33,6 @@ def create_lead():
             "visits": data['visits']
         }, 201
 
-    # TODO: tratar exceções do email e phone unicos => verificar o erro de retorno            
     except sqlalchemy.exc.IntegrityError as e:
         print(e.__dict__)
         return {'error': str(e.orig).split('\n')[1]}, 422
@@ -43,37 +41,28 @@ def create_lead():
 def get_all_leads():
     leads_list = Lead.query.all()
 
-    # TODO: tratar exceção usando a lista estiver vázia
     if leads_list == []:
         return {'msg': 'The list is empty!'}, 200
 
-    # TODO: deve ser ordenado pelo numero de visitas (maior para o menor)
     leads_list = sorted(leads_list, key=lambda item: item.visits)
 
     return jsonify(leads_list), 200
 
 
 def update_lead():
-    # TODO: corpo da requisição obrigatóriamente apenas com o email str
     data = request.get_json()
 
     try:
-        # TODO: usar o email para encontrar o registro
         lead_to_update = Lead.query.filter_by(email=data['email']).one()
-        print(lead_to_update)
         
-        # TODO: atualizar a data de 'las_visit' para o momento do request
         lead_to_update.last_visit = datetime.now()
 
-        # TODO: acrescentar em 1 o numero de visitas 'visits'
         lead_to_update.visits += 1
 
         current_app.db.session.commit()
 
-        # TODO: retorno vázio
         return '', 200
     
-    # TODO: tratar exceção de nenhum registro encontrado
     except sqlalchemy.exc.NoResultFound:
         return {'error': f"Could not find {data['email']} in the records!"}, 404
     
@@ -82,20 +71,16 @@ def update_lead():
 
 
 def delete_lead():
-    # TODO: corpo da requisição obrigatóriamente apenas com o email str
     data = request.get_json()
 
     try:
-        # TODO: usar o email para encontrar o registro
         lead_to_delete = Lead.query.filter_by(email=data['email']).one()
 
         current_app.db.session.delete(lead_to_delete)
         current_app.db.session.commit()
 
-        # TODO: retorno vázio
         return '', 200
     
-    # TODO: tratar exceção de nenhum registro encontrado
     except sqlalchemy.exc.NoResultFound:
         return {'error': f"Could not find {data['email']} in the records!"}, 404
 
